ce30cc58dcbb3056ea78864b2e263f92977bc9b1
Razvan PC <becheru.razvan@gmail.com>
2019-09-02 17:38:34 +0300

    ABOUT PAGE

--- js/custom.js
+++ js/custom.js
@@ -50,6 +50,7 @@ $(document).ready(function()
 	initDatePicker();
 	initTimePicker();
 	initMenu();
+	initAccordions()
 
 	/* 
 
@@ -148,6 +149,24 @@ $(document).ready(function()
 		    dropdown: true,
 		    scrollbar: true
 		});
+	}
+
+	function initAccordions()
+	{
+		var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+			acc[i].addEventListener("click", function() {
+				this.classList.toggle("active");
+				var panel = this.nextElementSibling;
+				if (panel.style.display === "block") {
+					panel.style.display = "none";
+				} else {
+					panel.style.display = "block";
+				}
+			});
+		}
 	}
 
 });
